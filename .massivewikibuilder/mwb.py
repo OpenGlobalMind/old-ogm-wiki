264dcd73538617d15c954d3024a181c424e6b830
Peter Kaminski <kaminski@istori.com>
2022-06-27 14:36:56 -0700

    upgrade to MWB v1.7.1

--- .massivewikibuilder/mwb.py
+++ .massivewikibuilder/mwb.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-# Massive Wiki Builder v1.2.1 - https://github.com/peterkaminski/massivewikibuilder
+# Massive Wiki Builder v1.7.1 - https://github.com/peterkaminski/massivewikibuilder
 
+# set up logging
+import logging, os
+logging.basicConfig(level=os.environ.get('LOGLEVEL', 'WARNING').upper())
+
+# python libraries
 import argparse
 import json
-import os
 import re
 import shutil
 import sys
@@ -17,31 +21,65 @@ import yaml
 import jinja2
 
 from markdown import Markdown
-from mdx_wikilink_plus.mdx_wikilink_plus import WikiLinkPlusExtension
+sys.path.append('./mwb_wikilink_plus/')
+from mwb_wikilink_plus.mwb_wikilink_plus import WikiLinkPlusExtension
 
 # set up argparse
 def init_argparse():
     parser = argparse.ArgumentParser(description='Generate HTML pages from Markdown wiki pages.')
+    parser.add_argument('--config', '-c', required=True, help='path to YAML config file')
     parser.add_argument('--output', '-o', required=True, help='directory for output')
     parser.add_argument('--templates', '-t', required=True, help='directory for HTML templates')
     parser.add_argument('--wiki', '-w', required=True, help='directory containing wiki files (Markdown + other)')
     return parser
 
+wikifiles = {}
+
+def mwb_build_wikilink(path, base, end, url_whitespace, url_case):
+    logging.debug("1 mwb_build_wikilink: path: %s", path)
+    path_name = Path(path).name
+    wikilink = Path(path_name).as_posix()  # use path_name if no wikipath
+    if path_name in wikifiles.keys():
+        wikipath = wikifiles[path_name]
+        logging.debug("2 mwb_build_wikilink: wikipath: %s", wikipath)
+        if wikipath.endswith('.md'):
+            wikilink = Path(wikipath).with_suffix('.html').as_posix()
+        else:
+            wikilink = Path(wikipath).as_posix()
+    logging.debug("3 mwb_build_wikilink return: %s", wikilink)
+    return wikilink
+
 # set up markdown
 markdown_configs = {
-    'mdx_wikilink_plus': {
+    'mwb_wikilink_plus': {
         'base_url': '',
         'end_url': '.html',
         'url_whitespace': '_',
+        'build_url': mwb_build_wikilink,
     },
 }
-markdown = Markdown(output_format="html5", extensions=[WikiLinkPlusExtension(markdown_configs['mdx_wikilink_plus'])])
+markdown_extensions = [
+    'footnotes',
+    'tables',
+    WikiLinkPlusExtension(markdown_configs['mwb_wikilink_plus']),
+]
+markdown = Markdown(output_format="html5", extensions=markdown_extensions)
 
 # set up a Jinja2 environment
 def jinja2_environment(path_to_templates):
     return jinja2.Environment(
         loader=jinja2.FileSystemLoader(path_to_templates)
     )
+
+# load config file
+def load_config(path):
+    with open(path) as infile:
+        return yaml.safe_load(infile)
+
+# scrub wiki path to handle ' ', '_', '?', and '#' characters in wiki page names
+# change ' ', ?', and '#' to '_', because they're inconvenient in URLs
+def scrub_path(filepath):
+    return re.sub(r'([ _?\#]+)', '_', filepath)
 
 # take a path object pointing to a Markdown file
 # return Markdown (as string) and YAML front matter (as dict)
@@ -69,20 +107,28 @@ def read_markdown_and_front_matter(path):
     # return Markdown + empty dict
     return ''.join(lines), {}
 
+# read and convert Sidebar markdown to HTML
+def sidebar_convert_markdown(path):
+    if path.exists():
+        markdown_text, front_matter = read_markdown_and_front_matter(path)
+    else:
+        markdown_text = ''
+    return markdown.convert(markdown_text)
+
 # handle datetime.date serialization for json.dumps()
 def datetime_date_serializer(o):
     if isinstance(o, datetime.date):
         return o.isoformat()
 
 def main():
+    logging.debug("Initializing")
+
     argparser = init_argparse();
     args = argparser.parse_args();
+    logging.debug("args: %s", args)
 
     # get configuration
-    wiki_title = "OGM Wiki"
-    author = "the Open Global Mind community"
-    repo = '<a href="https://github.com/openglobalmind/ogm-wiki">GitHub/openglobalmind/ogm-wiki</a>'
-    license = '<a href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>'
+    config = load_config(args.config)
 
     # remember paths
     dir_output = os.path.abspath(args.output)
@@ -95,22 +141,47 @@ def main():
     # render the wiki
     try:
         # remove existing output directory and recreate
+        logging.debug("remove existing output directory and recreate")
         shutil.rmtree(dir_output, ignore_errors=True)
         os.mkdir(dir_output)
 
+        # generate dict of filenames and their wikipaths
+        for root,dirs,files in os.walk(dir_wiki):
+            dirs[:]=[d for d in dirs if not d.startswith('.')]
+            files=[f for f in files if not f.startswith('.')]
+            readable_path = root[len(dir_wiki):]
+            path = scrub_path(readable_path)
+            for file in files:
+                if file in ['netlify.toml']:
+                    continue
+                clean_name = scrub_path(file)
+                if '.md' == Path(file).suffix.lower():
+                    wikifiles[Path(file).stem] = f"{path}/{clean_name}"
+                else:
+                    wikifiles[Path(file).name] = f"{path}/{clean_name}"
+        logging.debug("wikifiles: %s", wikifiles)
         # copy wiki to output; render .md files to HTML
+        logging.debug("copy wiki to output; render .md files to HTML")
         all_pages = []
         page = j.get_template('page.html')
         build_time = datetime.datetime.now(datetime.timezone.utc).strftime("%A, %B %d, %Y at %H:%M UTC")
+        if 'sidebar' in config:
+            sidebar_body = sidebar_convert_markdown(Path(dir_wiki) / config['sidebar'])
+        else:
+            sidebar_body = ''
         for root, dirs, files in os.walk(dir_wiki):
             dirs[:] = [d for d in dirs if not d.startswith('.')]
             files = [f for f in files if not f.startswith('.')]
             readable_path = root[len(dir_wiki)+1:]
-            path = re.sub(r'([ ]+_)|(_[ ]+)|([ ]+)', '_', readable_path)
+            path = scrub_path(readable_path)
             if not os.path.exists(Path(dir_output) / path):
                 os.mkdir(Path(dir_output) / path)
+            logging.debug("processing %s", files)
             for file in files:
-                clean_name = re.sub(r'([ ]+_)|(_[ ]+)|([ ]+)', '_', file)
+                logging.debug("main: processing: file: %s", file)
+                if 'sidebar' in config and file == config['sidebar']:
+                    continue
+                clean_name = scrub_path(file)
                 if file.lower().endswith('.md'):
                     # parse Markdown file
                     markdown_text, front_matter = read_markdown_and_front_matter(Path(root) / file)
@@ -121,27 +192,51 @@ def main():
                     (Path(dir_output) / path / clean_name).with_suffix(".json").write_text(json.dumps(front_matter, indent=2, default=datetime_date_serializer))
 
                     # render and output HTML
+                    markdown.reset() # needed for footnotes extension
                     markdown_body = markdown.convert(markdown_text)
-                    html = page.render(build_time=build_time, wiki_title=wiki_title, author=author, repo=repo, license=license, title=file[:-3], markdown_body=markdown_body)
+                    html = page.render(
+                        build_time=build_time,
+                        wiki_title=config['wiki_title'],
+                        author=config['author'],
+                        repo=config['repo'],
+                        license=config['license'],
+                        title=file[:-3],
+                        markdown_body=markdown_body,
+                        sidebar_body=sidebar_body
+                    )
                     (Path(dir_output) / path / clean_name).with_suffix(".html").write_text(html)
 
                     # remember this page for All Pages
                     all_pages.append({'title':f"{readable_path}/{file[:-3]}", 'path':f"{path}/{clean_name[:-3]}.html"})
                 # copy all original files
+                logging.debug("copy all original files")
                 shutil.copy(Path(root) / file, Path(dir_output) / path / clean_name)
 
         # copy README.html to index.html if no index.html
+        logging.debug("copy README.html to index.html if no index.html")
         if not os.path.exists(Path(dir_output) / 'index.html'):
             shutil.copyfile(Path(dir_output) / 'README.html', Path(dir_output) / 'index.html')
 
         # copy static assets directory
+        logging.debug("copy static assets directory")
         if os.path.exists(Path(dir_templates) / 'mwb-static'):
             shutil.copytree(Path(dir_templates) / 'mwb-static', Path(dir_output) / 'mwb-static')
 
         # build all-pages.html
+        logging.debug("build all-pages.html")
         all_pages = sorted(all_pages, key=lambda i: i['title'].lower())
-        html = j.get_template('all-pages.html').render(build_time=build_time, pages=all_pages, wiki_title=wiki_title, author=author, repo=repo, license=license)
+        html = j.get_template('all-pages.html').render(
+            build_time=build_time,
+            pages=all_pages,
+            wiki_title=config['wiki_title'],
+            author=config['author'],
+            repo=config['repo'],
+            license=config['license']
+        )
         (Path(dir_output) / "all-pages.html").write_text(html)
+
+        # done
+        logging.debug("done")
 
     except Exception as e:
         traceback.print_exc(e)
